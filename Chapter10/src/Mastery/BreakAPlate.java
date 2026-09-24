35871e1945abb96a8108ea2d8171fe8cebcd65b9
1100069235 <1100069235@L815WD220031.edu.cbe.ab.ca>
2026-09-24 10:01:13 -0600

    10:01 AM

--- Chapter10/src/Mastery/BreakAPlate.java
+++ Chapter10/src/Mastery/BreakAPlate.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -40,9 +41,16 @@ public class BreakAPlate {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize() 
+	{
+		ImageIcon allBroken = new ImageIcon("../Chapter10/src/Mastery/plates_all_broken.gif");
+		ImageIcon twoBroken = new ImageIcon("../Chapter10/src/Mastery/plates_two_broken.gif");
+		ImageIcon platesRegular = new ImageIcon("../Chapter10/src/Mastery/plates.gif");
+		ImageIcon sticker = new ImageIcon("../Chapter10/src/Mastery/sticker.gif");
+		ImageIcon tiger = new ImageIcon("../Chapter10/src/Mastery/tiger_plush.gif");
+		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 580, 350);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
@@ -50,15 +58,39 @@ public class BreakAPlate {
 		panel.setLayout(null);
 		
 		JLabel plates = new JLabel("");
-		plates.setBounds(45, 31, 342, 83);
+	
+		plates.setBounds(148, 24, 278, 90);
 		panel.add(plates);
 		
-		JButton play = new JButton("Click to Fart");
-		play.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		JLabel prize = new JLabel("");
+		prize.setBounds(230, 169, 108, 90);
+		panel.add(prize);
+		
+		JButton play = new JButton("Click to break");
+		play.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				int breakPlate;
+				
+				breakPlate = (int)(2 * Math.random() + 1);
+				
+				if(breakPlate == 1) 
+				{
+					plates.setIcon(twoBroken);
+					prize.setIcon(sticker);
+				}
+				
+				else if(breakPlate == 2) 
+				{
+					plates.setIcon(allBroken);
+					prize.setIcon(tiger);
+				}
 			}
 		});
-		play.setBounds(160, 164, 108, 33);
+		play.setBounds(210, 125, 145, 33);
 		panel.add(play);
+		
+		
 	}
 }
